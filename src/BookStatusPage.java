1eb403f1a50fc0ac305820454cf2c6ad758c4dad
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 <getahunlast@gmail.com>
2023-01-13 12:43:17 +0300

    book add function

--- src/BookStatusPage.java
+++ src/BookStatusPage.java
@@ -2,4 +2,5 @@ import javax.swing.*;
 
 public class BookStatusPage extends JDialog{
     public JPanel BookStatuspanel;
+    private JTable table1;
 }
--- src/NewBook.java
+++ src/NewBook.java
@@ -1,9 +1,62 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class NewBook extends JDialog {
     public JPanel NewBookPanel;
     private JTextField BookTitletxt;
     private JTextField Subjecttxt;
     private JTextField AuthorText;
-    private JButton submitButton;
+    private JButton Savebtn;
+    private JTextField ShelfNumberText;
+    private JTextField Quantity;
+    private JTextField IDtxt;
+    Connection connection;
+    Statement statement;
+
+
+    public NewBook() {
+
+    Savebtn.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String id=IDtxt.getText();
+            String BookTitle=BookTitletxt.getText();
+            String subject=Subjecttxt.getText();
+            String Author=AuthorText.getText();
+            String ShelfNumber=ShelfNumberText.getText();
+            String amount=Quantity.getText();
+            try {
+
+
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection= DriverManager.getConnection("jdbc:mysql://localhost/librarydb","root","root");
+                statement=connection.createStatement();
+               //String mysql="INSERT INTO  + "values("++','+"++','"++"','"++"','"++"','"++")"
+                String mysql="INSERT INTO books(id,title,subject,author,shelf_no,Quantity)" + "VALUES(?, ? ,? ,?,?,?)";
+                PreparedStatement ps=connection.prepareStatement(mysql);
+                ps.setString(1,id);
+                ps.setString(2,BookTitle);
+                ps.setString(3,subject);
+                ps.setString(4,Author);
+                ps.setString(5,ShelfNumber);
+                ps.setString(6,amount);
+                int addrows = ps.executeUpdate();
+
+                statement.close();
+                ps.close();
+
+
+                JOptionPane.showMessageDialog(NewBookPanel,"The book is added Successfully !");
+                    dispose();
+                connection.close();
+            } catch (ClassNotFoundException | SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+
+        }
+    });
+}
 }
