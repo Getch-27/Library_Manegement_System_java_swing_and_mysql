abd1b25a49dee1d1affc9e36e65e0a2fa54c9204
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 <getahunlast@gmail.com>
2023-01-14 23:03:25 +0300

    issue book search function add result on table new

--- src/issueBookPage.java
+++ src/issueBookPage.java
@@ -7,22 +7,25 @@ import java.util.Vector;
 
 public class issueBookPage extends JDialog{
     public JPanel IssueBookPanel;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField IDtext;
+    private JTextField FullnameText;
     private JComboBox DepartmentCmb;
-    private JTextField textField3;
+    private JTextField DateText;
     private JRadioButton maleRadioButton;
     private JRadioButton femaleRadioButton;
     private JButton issueButton;
     private JTextField SearchText;
     private JButton SearchButton;
     private JTable SearchTable;
+    private JTextField StBookName;
 
     public issueBookPage() {
     DepartmentCmb.addItem("Computer");
     DepartmentCmb.addItem("Accounting");
     DepartmentCmb.addItem("Managment");
-
+       ButtonGroup group=new ButtonGroup();
+       group.add(maleRadioButton);
+       group.add(femaleRadioButton);
         SearchButton.addActionListener(new ActionListener() {
             @Override
 
@@ -37,11 +40,17 @@ public class issueBookPage extends JDialog{
                    ResultSet resultSet = statement.executeQuery(mysql);
 
                     DefaultTableModel t=BuildTable(resultSet);
+                    StBookName.setText(String.valueOf(Searched));
                     SearchTable.setModel(t);
+                     long millis=System.currentTimeMillis();
+                     java.sql.Date date=new java.sql.Date(millis);
+                    DateText.setText(String.valueOf(date));
                    if(resultSet.next()){
 
                        JOptionPane.showMessageDialog(IssueBookPanel,"no such book");
 
+
+                   }else{
 
                    }
                     statement.close();
@@ -49,6 +58,27 @@ public class issueBookPage extends JDialog{
                 } catch (ClassNotFoundException | SQLException ex) {
                     throw new RuntimeException(ex);
                 }
+            }
+        });
+        issueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String  ID=IDtext.getText();
+                String FullName=FullnameText.getText();
+
+
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                   Connection  connection= DriverManager.getConnection("jdbc:mysql://localhost/librarydb","root","root");
+                   Statement statement=connection.createStatement();
+                   //ResultSet resultSet= statement.executeQuery("SELECT *FROM librarian WHERE username='"+username+"' and password='"+password+"'");
+
+                } catch (ClassNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
             }
         });
     }
@@ -66,6 +96,7 @@ public class issueBookPage extends JDialog{
             Vector<Object> vector=new Vector<>();
             for(int columnNumber=1;columnNumber<= columnCount;columnNumber++){
                 vector.add(resultSet.getObject(columnNumber));
+
             }
             tableData.add(vector);
 
